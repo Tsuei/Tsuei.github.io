c2e7914c94517e1e8b887038b1db67ccbbf9078a
Tsuei <1624033170@qq.com>
2025-03-22 22:46:43 +0800

    初改图标字色等细节

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -191,7 +191,7 @@ module.exports = {
 
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['link', { rel: 'icon', href: '/img/favicon_ekt.ico' }], //favicons，资源放在public文件夹
     [
       'meta',
       {
